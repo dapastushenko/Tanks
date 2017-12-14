ebf9a22b54e392a466b908e927b6e36326c10ffb
pastushenko_da <dapastushenko@yandex.ru>
2017-12-14 14:32:45 +0300

    убрано лишнее

--- src/display/Display.java
+++ src/display/Display.java
@@ -1,5 +1,9 @@
 package display;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -8,34 +12,73 @@ public class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void created(int width,int height, String title){
+    private static BufferedImage buffer;
+    private static int[] bufferDate;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    private static BufferStrategy bufferStrategy;
+
+    private static float delta = 0;
+    //temp end
+
+    public static void created(int width,int height, String title, int _clearColor, int numBuffers){
         if (created)
             return;
 
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas(){
-            @Override
-            public void paint(Graphics g) {
-                super.paint(g);
-                render(g);
-            }
-        };
-
+        content = new Canvas();
 
         Dimension size = new Dimension(width,height);
         content.setPreferredSize(size);
-        content.setBackground(Color.black);
+
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        buffer = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+        bufferDate = ((DataBufferInt)buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        ((Graphics2D)bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON); //сглаживание объектов
+        clearColor = _clearColor;
+
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+
+        created=true;
     }
+    public static void clear(){
+        Arrays.fill(bufferDate,clearColor);
+    }
+
+    public static void swapBuffers(){
+        Graphics g  = bufferStrategy.getDrawGraphics();
+        g.drawImage(buffer,0,0,null);
+        bufferStrategy.show();
+    }
+
+    public static Graphics2D getGraphics(){
+        return (Graphics2D)bufferGraphics;
+    }
+
+    public static void destroy(){
+        //уничтожение окна
+        if(!created)
+            return;
+
+        window.dispose();
+    }
+
+    /*
     public static void render(){
-content.repaint();
+        content.repaint();
     }
     private static void render(Graphics g){
-
+        g.setColor(Color.white);
+        g.fillOval(400-50,300-50,100,100);
     }
+*/
 }

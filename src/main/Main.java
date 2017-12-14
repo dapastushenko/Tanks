47e9589e3d5a0cadb1d8c6bc28d90d1d1bef9dbb
pastushenko_da <dapastushenko@yandex.ru>
2017-12-14 14:53:09 +0300

    удалено лишнее чтобы использовать свою матчасть

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import display.Display;
+import game.Game;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -8,20 +9,9 @@ import java.awt.event.ActionEvent;
 public class Main {
 
     public static void main(String[] args){
-        //hex 0123456789abcdef - обозначение цветов. int - max 0x(прозр)ff(red)ff(green)ff(blue)ff
-        Display.created(800,600,"Tanks",0xff00ff00,3);
+        Game tanks = new Game();
+        tanks.start();
 
-        Timer t = new Timer(1000 / 60,new AbstractAction(){
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Display.clear();
-                //временно
-                Display.render();
-                Display.swapBuffers();
-            }
-        });
-        t.setRepeats(true);
-        t.start();
     }
 }

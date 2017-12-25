fa637c67eae7f5e736704f1a9ae89683d2a45346
pastushenko_da <dapastushenko@yandex.ru>
2017-12-25 14:23:07 +0300

    reformat code

--- src/main/java/graphics/TextureAtlas.java
+++ src/main/java/graphics/TextureAtlas.java
@@ -6,13 +6,14 @@ import java.awt.image.BufferedImage;
 
 public class TextureAtlas {
     BufferedImage image;
-    public TextureAtlas(String imageName){
+
+    public TextureAtlas(String imageName) {
         image = ResourceLoader.loadImage(imageName);
     }
 
     public BufferedImage cut(int x, int y, int w, int h) {
         //режем изображение на куски, от гиморой а
-        return image.getSubimage(x,y,w,h);
+        return image.getSubimage(x, y, w, h);
 
     }
 }

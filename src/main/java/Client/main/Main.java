e1e1a8c01a1414b6420dea84d89e0777b9f284c7
dapastushenko <dapastushenko@yandex.ru>
2018-01-15 17:15:45 +0300

    net

---
+++ src/main/java/Client/main/Main.java
@@ -0,0 +1,13 @@
+package Client.main;
+
+import ClientServer.game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game tanks = new Game();
+        tanks.start();
+
+
+    }
+}
